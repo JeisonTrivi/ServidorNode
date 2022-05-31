b23fd96e777dba51d56bf1d44c3582d95898dd5e
Jeison Triviño <jeisontrivino0617@gmail.com>
2022-05-31 16:28:29 -0500

    Subir al servidor Heroku

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 const express = require('express');
 
+const multer = require('multer');
+const sharp = require('sharp');
+const fs = require('fs');
+
+const storageStrategy = multer.memoryStorage();
+const upload = multer({ storage: storageStrategy });
+
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-app.listen (3000);
+app.post('/imagen', upload.single('imagen') ,async (req, res) => {
+
+    const imagen = req.file;
+
+    // buffer de datos de la imagen con sharp
+    const prosessImage = sharp(imagen.buffer);
+    //Re escalado de foto
+    const resizedImage = prosessImage.resize(800, 800, {
+        // escalamos la foto desdtro de nuestras dimenciones especificadas
+        fit: 'contain',
+        background: '#fff',
+    });
+    //Buffer de Imagen Con Promesa 
+    const resizedImagenBuffer = await resizedImage.toBuffer();
+    //fs con foto reescalada
+    fs.writeFileSync('rutaImagen/resized.jpg', resizedImagenBuffer);
+
+    console.log(resizedImagenBuffer);
+
+    res.send([{resizeImage : resizedImagenBuffer}]);
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen (PORT, () => {
+    console.log('Server on port '+ PORT);
+});
